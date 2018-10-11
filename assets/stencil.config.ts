043a06d3f835fd43ddf770f5a84dcd7c3399e583
Matt Neel <requisite.zero@gmail.com>
2018-10-10 15:22:17 -1000

    Example component.

--- assets/stencil.config.ts
+++ assets/stencil.config.ts
@@ -1,14 +1,15 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'mycomponent',
-  outputTargets:[
-    {
-      type: 'dist'
-    },
-    {
-      type: 'www',
-      serviceWorker: null
-    }
-  ]
+	namespace: 'mycomponent',
+	outputTargets: [
+		{
+			type: 'dist'
+		},
+		{
+			type: 'www',
+			dir: '../priv/static',
+			serviceWorker: null
+		}
+	]
 };
